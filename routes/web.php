c005351d1ed961768069ae2662f745c1f89f2ca5
Ray <rudiadriian@gmail.com>
2026-01-12 10:18:03 +0700

    Add debug route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/cek-file', function () {
+    $files = scandir(public_path());
+    return [
+        'path_public' => public_path(),
+        'isi_folder' => $files
+    ];
+});

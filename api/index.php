7c94621a510c39a1bcaebf698ca372d31c3a02b0
Ray <rudiadriian@gmail.com>
2026-01-12 11:24:41 +0700

    Final Fix: Other framework preset and exact routing

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+$app->useStoragePath('/tmp');
 
-require __DIR__ . '/../public/index.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
